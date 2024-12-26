a7288f6edf0351c730e8ccca6038bc668048d7ae
Pramod22nov <pramodsharma9693@gmail.com>
2024-12-26 17:21:16 +0530

    connect mongo server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Successfully connect")
+    }).catch((err) => {
+        console.log(err)
+    });
 
 const app = express();
 
