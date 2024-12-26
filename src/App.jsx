91fbb0884c42d8f9d02ecd3841c04fd5ef7df167
Pramod22nov <pramodsharma9693@gmail.com>
2024-12-26 15:56:30 +0530

    Create Page and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Singin from "./pages/Singin"
+import Singout from "./pages/Singout"
 
 export default function App() {
-  return (
-    <div class="text-red-900 ">
-      App
-    </div>
-  )
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/singin" element={<Singin />}/>
+        <Route path="/singout" element={<Singout />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Singout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Singout() {
+  return (
+    <div>
+      singout
+    </div>
+  )
+}
